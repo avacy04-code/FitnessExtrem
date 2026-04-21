57333f5199a59c35b8e6a0eb8980b81b90fa386f
avacy04-code <a.vacy04@gmail.com>
2026-04-21 17:50:26 +0200

    Add console log for FitnessExtrem

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const menuToggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav');
-
-if (menuToggle && nav) {
-  menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-
-  nav.querySelectorAll('a').forEach((link) => {
-    link.addEventListener('click', () => {
-      nav.classList.remove('active');
-    });
-  });
-}
+console.log("FitnessExtrem listo");
